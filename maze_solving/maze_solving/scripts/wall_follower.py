d169073187ba55d638694329174a169d40c79411
Charifou OROU MOUSSE <c.oroumousse@sherpa-eng.com>
2025-01-29 23:09:00 +0100

    maze solved with nav2

--- maze_solving/maze_solving/scripts/wall_follower.py
+++ maze_solving/maze_solving/scripts/wall_follower.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 import rclpy
+import rclpy.logging
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
+import numpy as np
 
 class WallFollower(Node):
     def __init__(self):
@@ -16,13 +18,62 @@ class WallFollower(Node):
         self.obstacle_distance_threshold = 0.3  # m
         self.turn_speed = 0.5  # Angular speed for turns (rad/s)
         self.forward_speed = 0.2  # Linear speed (m/s)
-
-        self.follow_left_wall = True  
+        self.follow_left_wall = True
+        self.angle_min = 0.0
+        self.angle_increment = 0.0
+        self.angle_max = 0.0
+        self.front_angle = np.pi / 16  # 10 degrees  
+        self.first_read = False
 
     def lidar_callback(self, msg: LaserScan):
+        
+        # self.get_logger().info()
+        if(not self.first_read):
+           self.angle_min = msg.angle_min
+           self.angle_max = msg.angle_max
+           self.angle_increment = msg.angle_increment
+        
         # Extract key distances from LiDAR scan
-        # TODO
-        return
+        front_indices = self.calculate_indices(-self.front_angle, +self.front_angle)
+        left_indices = self.calculate_indices(self.angle_min, -self.front_angle) 
+        right_indices = self.calculate_indices(self.front_angle, self.angle_max)
+
+        # Extract ranges
+        front_ranges = msg.ranges[front_indices[0]:front_indices[1]]
+        left_ranges = msg.ranges[left_indices[0]:left_indices[1]]
+        right_ranges = msg.ranges[right_indices[0]:right_indices[1]]
+
+        front_range_min = min(front_ranges)
+        front_range_max = max(front_ranges)
+        left_range_min  =  min(left_ranges)
+        left_range_max  =  max(left_ranges)
+        right_range_min = min(right_ranges)
+        right_range_max = max(right_ranges)
+
+        print(front_range_min)
+        print(front_range_max)
+        # print(left_range_min )
+        # print(left_range_max )
+        # print(right_range_min)
+        # print(right_range_max)
+        # if (front_range_max) > 1.0:
+        #     print(front_range_max)
+        #     self.move_forward()
+        # else:
+        #     self.stop()
+        
+        # self.get_logger().info(f"Front ranges: {front_ranges}")
+        # self.get_logger().info(f"Left ranges: {left_ranges}")
+        # self.get_logger().info(f"Right ranges: {right_ranges}")
+        
+
+        
+    
+    def calculate_indices(self, start_angle, end_angle):
+        start_index = int((start_angle - self.angle_min) / self.angle_increment)
+        end_index = int((end_angle - self.angle_min) / self.angle_increment)
+        return start_index, end_index
+
 
     def move_forward(self):
         
@@ -56,6 +107,13 @@ class WallFollower(Node):
         twist.linear.x = self.forward_speed
         twist.angular.z = -0.2  
         self.cmd_vel_publisher.publish(twist)
+    
+    def stop(self):
+        twist = Twist()
+        twist.linear.x = 0.0
+        twist.angular.z = 0.0  
+        self.cmd_vel_publisher.publish(twist)
+    
 
 
 def main(args=None):
